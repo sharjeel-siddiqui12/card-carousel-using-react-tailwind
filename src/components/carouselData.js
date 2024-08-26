4db25b06f30972546bcd93efd5dca4684a1c4d2c
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-08-27 00:31:28 +0500

    Refactor image paths in carouselData.js

--- src/components/carouselData.js
+++ src/components/carouselData.js
@@ -1,26 +1,29 @@
 // carouselData.js
 import building1 from '../assets/building/building1.jpg';
+import building2 from '../assets/building/building2.jpg';
+import building4 from '../assets/building/building4.jpg';
+import building5 from '../assets/building/building5.jpg';
 
 const carouselData = [
     {
+      name: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis',
+      img: building1     
+     },
+    {
       name: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis`,
-      img: {building1}
+      img: building2  
     },
     {
       name: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis`,
-      img: `./src/assets/building/building2.jpg`,
+      img: building4  
     },
     {
       name: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis`,
-      img: `./src/assets/building/building5.jpg`,
+      img: building5  
     },
     {
       name: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis`,
-      img: `./src/assets/building/building4.jpg`,
-    },
-    {
-      name: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium sunt repellendus officiis`,
-      img: `./src/assets/building/building5.jpg`,
+      img: building1  
     },
   ];
   
